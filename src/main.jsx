7c77b78d6710dfbd0534fedcf9ec8586362afca7
davydenko919 <davydenko919@gmail.com>
2025-05-22 12:07:12 +0300

    start

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import './index.css';
+import App from './components/App/App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+// import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      {/* <Provider store={store}>
+        <PersistGate persistor={persistor}> */}
+          <App />
+        {/* </PersistGate>
+      </Provider> */}
+    </BrowserRouter>
+  </React.StrictMode>
+);
